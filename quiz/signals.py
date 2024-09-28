c59ccfb56c67708c6933e590e792e36430219b17
Oleh Smolynets <smolynets@gmail.com>
2024-09-28 16:53:02 +0300

    register signals

--- quiz/signals.py
+++ quiz/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import pre_save
 from django.core.exceptions import ValidationError
+from django.dispatch import receiver
 
-from quiz.models import Questionare, Question
+from quiz.models import Questionare, Question, Answer
 
 @receiver(pre_save, sender=Questionare)
 def check_minimum_questions(sender, instance, **kwargs):
@@ -16,5 +17,11 @@ def validate_answers(sender, instance, **kwargs):
         raise ValidationError("Має бути рівно одна правильна відповідь на запитання.")
 
 
+def validate_answer(sender, instance, **kwargs):
+    if instance.is_correct:
+        raise ValidationError("Кожне запитbjhbjhbjнти відповідей.")
+
+
 pre_save.connect(check_minimum_questions, sender=Questionare)
-post_save.connect(validate_answers, sender=Question)
+pre_save.connect(validate_answers, sender=Question)
+pre_save.connect(validate_answer, sender=Answer)
